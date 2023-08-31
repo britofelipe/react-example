7299cef487a0729ffc1eaca51d520e4475b565d5
Felipe Brito <arrudabritofelipe@gmail.com>
2023-08-31 18:44:46 -0300

    Constructing components

---
+++ src/components/Comment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Comment.css'
+
+const Comment = () => (
+    <div className="Comment">
+        <div>John: </div>
+        <div>This is a component</div>
+    </div>
+);
+
+export default Comment;
